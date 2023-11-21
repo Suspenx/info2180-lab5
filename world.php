7d0b610850be023da7d95a4275b8a54fffb6d311
Suspenx <vaughndalespence@gmail.com>
2023-11-20 20:02:55 -0500

    Get Request php

--- world.php
+++ world.php
@@ -1,17 +1,28 @@
 <?php
+
+header("Access-Control-Allow-Origin: *"); 
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($_SERVER['REQUEST_METHOD']=== "GET") {
+  if(isset($_GET["country"])){
+    $country = $_GET["country"];
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  }
+  else {
+    $stmt = $conn->query("SELECT * FROM countries");
+  }
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 ?>
-<ul>
+ <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul> 
