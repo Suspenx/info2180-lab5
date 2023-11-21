2f63e198e5af446d3dba212c564e9f09db83d450
Suspenx <vaughndalespence@gmail.com>
2023-11-20 20:42:48 -0500

    Get country

---
+++ world.js
@@ -0,0 +1,17 @@
+ document.addEventListener("DOMContentLoaded",()=>{
+    
+     const lookup_button = document.getElementById("lookup")
+     const country= document.getElementById("country")
+     const results = document.getElementById("result")
+
+    lookup_button.addEventListener("click",async()=>{
+         input = country.value
+         const response= await fetch(`http://localhost/info2180-lab5/world.php?country=${input}`)
+         const feedback = await response.text()
+         results.innerHTML = feedback
+     })
+
+ })
+
+
+ 
